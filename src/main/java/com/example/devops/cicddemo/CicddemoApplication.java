91c4ee4c4c3938b5170f3378d52c126d78c52521
dmnglore <35909263+dmnglor@users.noreply.github.com>
2023-09-26 17:52:36 +0530

    updated main

--- src/main/java/com/example/devops/cicddemo/CicddemoApplication.java
+++ src/main/java/com/example/devops/cicddemo/CicddemoApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CicddemoApplication {
 
-	public static void main(String[] args) {
+	public static void (String[] args) {
 		SpringApplication.run(CicddemoApplication.class, args);
 
 	}
